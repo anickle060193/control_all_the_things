9cc6105220b408fe7e2021f3429b093713504f44
Adam Nickle <anickle060193@gmail.com>
2016-12-23 11:58:04 -0600

    Initial commit of application.

---
+++ app/ControlAllTheThings/ControlAllTheThingsForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlAllTheThings
+{
+    public partial class ControlAllTheThingsForm : Form
+    {
+        public ControlAllTheThingsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
