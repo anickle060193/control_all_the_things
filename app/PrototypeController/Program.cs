f9c971c5fd7d613da73b830dedf2ac9db45902bb
Adam Nickle <anickle060193@gmail.com>
2017-01-03 22:48:39 -0600

    Added exception catcher for release builds.

--- app/PrototypeController/Program.cs
+++ app/PrototypeController/Program.cs
@@ -16,7 +16,18 @@ namespace PrototypeController
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault( false );
+#if DEBUG
             Application.Run( new PrototypeControllerForm() );
+#else
+            try
+            {
+                Application.Run( new PrototypeControllerForm() );
+            }
+            catch( Exception e )
+            {
+                MessageBox.Show( "The following error has occured:\n" + e.ToString(), "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error );
+            }
+#endif
         }
     }
 }
