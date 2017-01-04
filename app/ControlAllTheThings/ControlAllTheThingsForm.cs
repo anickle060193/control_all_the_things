32c00030e7346636c6ceb2ee4bf6b9bb1770bcc7
Adam Nickle <anickle060193@gmail.com>
2017-01-03 20:25:25 -0600

    Fixed index out-of-bounds exception.

--- app/ControlAllTheThings/ControlAllTheThingsForm.cs
+++ app/ControlAllTheThings/ControlAllTheThingsForm.cs
@@ -129,7 +129,7 @@ namespace ControlAllTheThings
 
         private void Board_Startup( Object sender, DoWorkEventArgs e )
         {
-            for( int i = 0; i < _board.OutputPins.Count + 1; i++ )
+            for( int i = 0; i < _board.OutputPins.Count; i++ )
             {
                 if( i - 1 >= 0 )
                 {
